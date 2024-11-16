5f5d8dbda879891e721a26d397770579293ed3ad
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-11-16 21:23:07 +0100

    total markup

---
+++ src/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+dotenv.config();
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).send({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (contact === null) {
+      return res
+        .status(404)
+        .send({ status: 404, message: 'Contact not found' });
+    }
+
+    res.status(200).send({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).send({ status: 404, message: 'Not found' });
+  });
+
+  app.use((err, _req, res, _next) => {
+    res.status(500).send({ status: 500, message: 'Internal server error' });
+  });
+
+  const PORT = Number(process.env.PORT) || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
